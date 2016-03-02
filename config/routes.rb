127dda7d3f90a995266af4b4b6447146121b95a5
dilliraja-coax <dilliraja.coax@gmail.com>
2016-03-03 00:30:29 +0530

    routes upts

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-  root 'application#coax'
+   root 'users#index'
+  #root 'application#coax'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
